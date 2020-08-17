ac5cec7e49b986abba55c952026ce0e5bca5581b
neulhan <neulhan@lawandgood.com>
2020-08-17 22:50:29 +0900

    명언 추가, 시간 추가 완료

---
+++ public/js/index.js
@@ -0,0 +1,21 @@
+const getCookie = (what) => {
+  if (what === "background") {
+    return "../src/image/default-background.jpg";
+  }
+};
+const setTime = () => {
+  const d = new Date();
+  const timeEl = document.getElementById("main-time");
+  timeEl.innerText = `${d.getFullYear()}년 ${d.getMonth()}월 ${d.getDate()}일 ${d.getHours()}시 ${d.getMinutes()}분`;
+};
+
+window.onload = () => {
+  const root = document.getElementById("root");
+  const background = getCookie("background");
+  const wordEl = document.getElementById("word");
+  root.setAttribute("style", `background-image: url("${background}")`);
+  setInterval(setTime, 1000);
+  const randomWord =
+    config.wordList[Math.floor(Math.random() * config.wordList.length)];
+  wordEl.innerText = randomWord;
+};
