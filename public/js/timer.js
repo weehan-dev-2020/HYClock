875f0f7dd5c2edea1c1c8f2959f2e23fee66f537
Jung <jungeun.lak@gmail.com>
2020-08-27 18:44:06 +0900

    학번 저장

--- public/js/timer.js
+++ public/js/timer.js
@@ -1,18 +1,65 @@
-const setTimer = (dDay) => {
-    const now = new Date();
-    const time = dDay - now;
-    const date = Math.floor(time / (1000 * 60 * 60 * 24));
-    const hour = Math.floor((time / (1000 * 60 * 60)) % 24);
-    const min = Math.floor((time / (1000 * 60)) % 60);
-    const sec = Math.floor((time / 1000) % 60);
-    const timerEl = document.getElementById("timer");
-    timerEl.innerText = `${date}일 ${hour}시 ${min}분 ${sec}초`;
+const saveGrade = (gradeInput) => {
+    try {
+        console.log("saveGrade");
+        gradeInput = parseInt(gradeInput);
+        console.log(gradeInput)
+        localStorage.setItem("univclock-userGrade", gradeInput);
+    }
+    catch (e) {
+        console.log("saveGrade, catch E");
+        gradeInput = 'NaN';
+    }
+}
+
+const submitHandle = () => {
+    console.log("submitHandle");
+    const gradeForm = document.querySelector(".grade");
+    const gradeInput = gradeForm.querySelector("select").value;
+    saveGrade(gradeInput);
+}
+
+const askGrade = () => {
+    console.log("askGrade");
+    const gradeForm = document.querySelector(".grade");
+    gradeForm.classList.remove("invisible");
+    gradeForm.addEventListener("submit", saveGrade);
+}
+
+const setGrade = () => {
+    const grade = localStorage.getItem("univclock-userGrade");
+    if (grade === 'NaN' || grade === null) {
+        console.log(1)
+        const gradeInput = askGrade();
+        saveGrade(gradeInput);
+    } else {
+        console.log(2)
+    }
 };
 
-const updateTimer = () => {
+const setStopwatch = (startDay) => {
+    const now = new Date();
+    const time = now - startDay;
+    const date = Math.floor(time / (1000 * 60 * 60 * 24));
+    const timerEl = document.getElementById("stopwatch");
+    timerEl.innerText = `+ ${date}일`;
+};
+
+const setTimer = (dDay) => {
+    const now = new Date();
+    const time = new Date(dDay - now);
+    const date = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hour = Math.floor((time / (1000 * 60 * 60)) % 24);
+    const timerEl = document.getElementById("timer");
+    timerEl.innerText = `${date}일 ${hour}시간 ${time.getMinutes()}분 ${time.getSeconds()}초`;
+};
+
+const loadTimer = () => {
     const dDay = new Date(2020, 11, 21);
+    const enterDay = new Date(2020, 01, 24);
     setTimer(dDay);
+    setGrade();
+    setStopwatch(enterDay);
     setInterval(setTimer, 1000, dDay);
 };
 
-window.addEventListener("load", updateTimer);
+window.addEventListener("load", loadTimer);
