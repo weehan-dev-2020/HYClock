5a5f5e45977f66fc22d028c0de9e2734744fcee4
neulhan <neulhan@lawandgood.com>
2020-09-16 13:24:43 +0900

    Bug(month): getMonth + 1

--- public/js/index.js
+++ public/js/index.js
@@ -168,7 +168,9 @@ const setName = () => {
 const setTime = () => {
   const d = new Date();
   const timeEl = document.getElementById("main-time");
-  timeEl.innerText = `${d.getFullYear()}년 ${d.getMonth()}월 ${d.getDate()}일 ${d.getHours()}시 ${d.getMinutes()}분,`;
+  timeEl.innerText = `${d.getFullYear()}년 ${
+    d.getMonth() + 1
+  }월 ${d.getDate()}일 ${d.getHours()}시 ${d.getMinutes()}분,`;
 };
 
 const getWordList = () => {
